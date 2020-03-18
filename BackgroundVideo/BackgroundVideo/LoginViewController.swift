9ad8146a19bba7f6a336840413864d7da18f0e82
DhakaLive <dhakalive.tv@Dhakalive-MBP2.local>
2020-03-18 16:21:22 +0600

    Update design

---
+++ BackgroundVideo/BackgroundVideo/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  BackgroundVideo
+//
+//  Created by DhakaLive on 18/3/20.
+//  Copyright © 2020 DhakaLive. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
